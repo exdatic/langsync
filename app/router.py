42cd652968f70c828207121e41ea549d35d126f3
Mark Reinke <mark@exdatis.ai>
2024-02-13 14:36:03 +0100

    add testset creation to langsync

--- app/router.py
+++ app/router.py
@@ -1,16 +1,21 @@
 from datetime import datetime
+import io
 import json
 import os
 from typing import Annotated
 
 from elasticsearch import Elasticsearch
 from fastapi import APIRouter, File, HTTPException, UploadFile
+from fastapi.responses import StreamingResponse
 from langchain.embeddings import CacheBackedEmbeddings
+from langchain.schema import Document
 from langchain.storage import LocalFileStore
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import ElasticsearchStore
 from langchain_openai import AzureOpenAIEmbeddings, OpenAIEmbeddings
 from loguru import logger
+from ragas.testset.generator import TestsetGenerator
+from ragas.testset.evolutions import simple, multi_context, reasoning
 
 
 router = APIRouter(tags=["API"])
@@ -44,21 +49,7 @@ def sync(
     query_field: str = "text",
     timeout: int = 300
 ):
-    texts = []
-    metadatas = []
-    for upload_file in jsonl_file:
-        for line in upload_file.file.readlines():
-            doc = json.loads(line)
-            if "content" not in doc:
-                raise HTTPException(400, detail="JSON lines entry must contain 'content' field containing the text")
-            if "source" not in doc:
-                raise HTTPException(400, detail="JSON lines entry must contain 'source' field containing the source url")
-            if "title" not in doc:
-                raise HTTPException(400, detail="JSON lines entry must contain 'title' field containing the title")
-            texts.append("\n\n".join([doc["title"], doc["content"]]))
-            metadata = {k: v for k, v in doc.items() if k not in ["content"]}
-            metadata["filename"] = upload_file.filename
-            metadatas.append(metadata)
+    texts, metadatas = load_files(jsonl_file)
 
     if not texts:
         raise HTTPException(400, detail="No content found in JSON lines file")
@@ -91,3 +82,51 @@ def sync(
     logger.info(f"Indexed {len(chunks)} chunks")
     publish_index(es, real_index_name, index_name)
     return "Updated data"
+
+
+@router.post("/testset", 
+             description="Create a testset with data from JSON lines files", 
+             response_class=StreamingResponse)
+def create_testset(
+    jsonl_file: Annotated[list[UploadFile],
+                          File(description="JSON lines file containing 'content', 'source' and 'title' fields")],
+    test_size: int,
+    distribution_simple: float = 0.75,
+    distribution_multi: float = 0.25,
+    distribution_reasoning: float = 0.0,
+    generator_llm: str = "gpt-35-turbo",
+    critic_llm: str = "gpt-4-turbo"
+):
+    texts, metadatas = load_files(jsonl_file)
+    documents = []
+    for text, metadata in zip(texts, metadatas):
+        metadata["filename"] = metadata["source"]
+        documents.append(Document(page_content=text, metadata=metadata))
+
+    generator = TestsetGenerator.with_openai(generator_llm=generator_llm, critic_llm=critic_llm)
+    testset = generator.generate_with_langchain_docs(documents, test_size, distributions={
+        simple: distribution_simple, multi_context: distribution_multi, reasoning: distribution_reasoning})
+    testset_df = testset.to_pandas()
+    stream = io.StringIO(testset_df.to_csv(index=False))
+    response = StreamingResponse(iter([stream.getvalue().encode()]), media_type="text/csv")
+    response.headers["Content-Disposition"] = "attachment; filename=testset.csv"
+    return response
+
+
+def load_files(jsonl_file):
+    texts = []
+    metadatas = []
+    for upload_file in jsonl_file:
+        for line in upload_file.file.readlines():
+            doc = json.loads(line)
+            if "content" not in doc:
+                raise HTTPException(400, detail="JSON lines entry must contain 'content' field containing the text")
+            if "source" not in doc:
+                raise HTTPException(400, detail="JSON lines entry must contain 'source' field containing the source url")
+            if "title" not in doc:
+                raise HTTPException(400, detail="JSON lines entry must contain 'title' field containing the title")
+            texts.append("\n\n".join([doc["title"], doc["content"]]))
+            metadata = {k: v for k, v in doc.items() if k not in ["content"]}
+            metadata["filename"] = upload_file.filename
+            metadatas.append(metadata)
+    return texts, metadatas
